eff78c5345bf4eaf0fb8f463e3aa1381a9a6a525
Jose Alvarez <jose@stylesage.co>
2019-12-06 22:56:29 +0100

    Added flask server

---
+++ python/flask/server.py
@@ -0,0 +1,75 @@
+import json
+
+from flask import Flask, jsonify, request
+from functools import wraps
+
+from python.core.basket import BasketManager
+from python.core.messages import MessageManager
+
+app = Flask(__name__)
+
+manager = BasketManager(
+    data_file_path='/Users/jose/Development/lana-go-challenge/data/products.json'
+)
+
+
+def message_to_json(f):
+    @wraps(f)
+    def wrapper(*args, **kw):
+        return jsonify(
+            json.loads(
+                f(*args, **kw).serialize()
+            )
+        )
+    return wrapper
+
+
+@app.route("/create_basket/", methods=['GET'])
+@message_to_json
+def create_basket():
+    basket_code = manager.create()
+    return MessageManager.create_basket_message(basket_code)
+
+
+@app.route("/remove_basket/<basket_code>/", methods=['GET'])
+@message_to_json
+def remove_basket(basket_code):
+    manager.remove(basket_code)
+    return MessageManager.create_ack_message()
+
+
+@app.route("/add_product/<basket_code>/<product_code>/", methods=['GET'])
+@message_to_json
+def add_product(basket_code, product_code):
+    manager.add_product(basket_code, product_code)
+    return MessageManager.create_ack_message()
+
+
+@app.route("/remove_product/<basket_code>/<product_code>/", methods=['GET'])
+@message_to_json
+def remove_product(basket_code, product_code):
+    manager.remove_product(basket_code, product_code)
+    return MessageManager.create_ack_message()
+
+
+@app.route("/get_products/<basket_code>/", methods=['GET'])
+@message_to_json
+def get_products(basket_code):
+    products = manager.get_products(basket_code)
+    return MessageManager.get_products_message(products=products)
+
+
+@app.route("/checkout/<basket_code>/", methods=['GET'])
+@message_to_json
+def checkout(basket_code):
+    total = manager.checkout(basket_code)
+    return MessageManager.get_checkout_message(total=total)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': '404 Not Found'}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
