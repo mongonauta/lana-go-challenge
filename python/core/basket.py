b804e5bb77b3499e1c620ab42a921a614ff49bae
Jose Alvarez <jose@stylesage.co>
2019-12-06 22:20:48 +0100

    Added docstring of the core

--- python/core/basket.py
+++ python/core/basket.py
@@ -4,6 +4,12 @@ from .inventory import Inventory
 
 
 class BasketItem(object):
+    """
+    This item is a basket representation.
+
+    code: uuid. Unique identifier for a basket
+    products: list of products in the basket
+    """
     code = None
     _products = None
 
@@ -12,24 +18,55 @@ class BasketItem(object):
         self._products = []
 
     def add_to_basket(self, item):
+        """
+        Add new item/product to the basket
+
+        item: Product item
+        return:
+        """
         self._products.append(item)
 
     def remove_from_basket(self, item):
+        """
+        Remove item from the basket
+
+        item: Product item
+        return:
+        """
         self._products.pop(item)
 
     def get_products(self):
+        """
+        Returns all the codes of all items inside the basket
+
+        return: Array of codes (str)
+        """
         return [item.code for item in self._products]
 
     def total_amount(self):
+        """
+        Returns the full price of the basket based on the price of every product and applying the discounts
+
+        return: total price of all products - discounts (float)
+        """
         full_price = sum(item.price for item in self._products)
         return full_price - self._get_discount()
 
     def _get_discount(self):
-        number_of_pens = len([x for x in self._products if x.code == 'PEN'])
-        number_of_tshirts = len([x for x in self._products if x.code == 'TSHIRT'])
+        """
+        Based on the products in the basket and discounts logic, calculates the discount
 
+        TODO: This function is too specific and not flexible. Refactor needed
+
+        return: total discount (float)
+        """
+
+        # For every 2 PENS, one free discount
+        number_of_pens = len([x for x in self._products if x.code == 'PEN'])
         discount = 5.0 * int(number_of_pens / 2)
 
+        # If there are more than 3 T-Shirts in the basket, 5 EUR of discount in every of them (25%)
+        number_of_tshirts = len([x for x in self._products if x.code == 'TSHIRT'])
         if number_of_tshirts >= 3:
             discount += 5.0 * number_of_tshirts
 
@@ -37,37 +74,91 @@ class BasketItem(object):
 
     @classmethod
     def create(cls):
+        """
+        Creates and empty basket with an auto generated UUID as code
+        """
         return BasketItem(code=str(uuid.uuid4()))
 
 
 class BasketManager(object):
+    """
+    Handles all baskets
+
+    inventory: list of available products
+    baskets: list of all baskets
+    """
     _inventory = None
     _baskets = None
 
     def __init__(self, data_file_path):
+        """
+        Constructor. Loads all products in the inventory and initializes the baskets container
+        """
         self._inventory = Inventory(data_file_path)
         self._baskets = {}
 
     def create(self):
+        """
+        Adds a new basket and returns the code for future location
+
+        return: UUID
+        """
         item = BasketItem.create()
         self._baskets[item.code] = item
         return item.code
 
     def remove(self, code):
+        """
+        Removes a basket.
+
+        code: UUID to locate the basket to remove
+
+        return:
+        """
         self._baskets.pop(code)
 
     def add_product(self, code, product_code):
+        """
+        Add a product from the inventory to a basket
+
+        code: UUID/code of the basket
+        product_code: code of the product to add
+
+        return:
+        """
         self._baskets[code].add_to_basket(
             item=self._inventory.get_item_by_code(code=product_code)
         )
 
     def remove_product(self, code, product_code):
+        """
+        Removed a product from a basket
+
+        code: UUID/code of the basket
+        product_code: code of the product to add
+
+        return:
+        """
         self._baskets[code].remove_from_basket(
             item=self._inventory.get_item_by_code(code=product_code)
         )
 
     def get_products(self, code):
+        """
+        Get all code products from a basket.
+
+        code: UUID/code of the basket
+
+        return:
+        """
         return self._baskets[code].get_products()
 
     def checkout(self, code):
+        """
+        Calculate the total value of a basket, including discounts.
+
+        code: UUID/code of the basket
+
+        return: float
+        """
         return self._baskets[code].total_amount()
--- python/core/messages.py
+++ python/core/messages.py
@@ -2,6 +2,11 @@ import json
 
 
 class Message(object):
+    """
+    This class tries to be a helper to keep consistent between client and servers of the app.
+
+    The app only understand this class objects BUT, using the serializers, we can use the objects in sockets, APIs...
+    """
     INVALID_CODE_MESSAGE = -1
     ACK_MESSAGE = 0
 
@@ -18,10 +23,21 @@ class Message(object):
     content = None
 
     def __init__(self, code, content):
+        """
+        Constructor. Every message must have a valid code and a content, different from every type of message.
+
+        code: valid message code. int
+        content: content of the message. Can be whatever BUT must be json serializable.
+        """
         self.code = code
         self.content = content
 
     def serialize(self):
+        """
+        Transforms the message in a valid JSON string
+
+        return: json str
+        """
         return json.dumps(
             {
                 'code': self.code,
@@ -31,6 +47,13 @@ class Message(object):
 
     @classmethod
     def deserialize(cls, s):
+        """
+        Transform a valid json (with the right format) in a Message object.
+
+        s: json str
+
+        return: Message
+        """
         json_obj = json.loads(s)
         return Message(
             code=json_obj['code'],
@@ -39,8 +62,16 @@ class Message(object):
 
 
 class MessageManager(object):
+    """
+    Helper manager to create right Message objects
+    """
     @classmethod
     def create_ack_message(cls):
+        """
+        Confirmation Message
+
+        return: Message object
+        """
         return Message(
             code=Message.ACK_MESSAGE,
             content=None
@@ -48,13 +79,27 @@ class MessageManager(object):
 
     @classmethod
     def create_invalid_message(cls):
+        """
+        Invalid code Message
+
+        return: Message object
+        """
         return Message(
             code=Message.INVALID_CODE_MESSAGE,
             content=None
         )
 
     @classmethod
-    def create_basket_message(cls, basket_code):
+    def create_basket_message(cls, basket_code=None):
+        """
+        Create a *create basket* Message.
+        - If *basket_code* is None, is a client request.
+        - If *basket_code* has a value, is a server response.
+
+        basket_code: UUID
+
+        return: Message object
+        """
         return Message(
             code=Message.CREATE_BASKET_MESSAGE,
             content={
@@ -64,6 +109,13 @@ class MessageManager(object):
 
     @classmethod
     def remove_basket_message(cls, basket_code):
+        """
+        Create a *remove basket* Message.
+
+        basket_code: UUID
+
+        return: Message object
+        """
         return Message(
             code=Message.REMOVE_BASKET_MESSAGE,
             content={
@@ -73,6 +125,14 @@ class MessageManager(object):
 
     @classmethod
     def add_product_message(cls, basket_code, product_code):
+        """
+        Create a *add product to basket* Message.
+
+        basket_code: UUID
+        product_code: Code of a product (str)
+
+        return: Message object
+        """
         return Message(
             code=Message.ADD_PRODUCT_MESSAGE,
             content={
@@ -83,6 +143,14 @@ class MessageManager(object):
 
     @classmethod
     def remove_product_message(cls, basket_code, product_code):
+        """
+        Create a *remove product from a basket* Message.
+
+        basket_code: UUID
+        product_code: Code of a product (str)
+
+        return: Message object
+        """
         return Message(
             code=Message.REMOVE_PRODUCT_MESSAGE,
             content={
@@ -93,6 +161,17 @@ class MessageManager(object):
 
     @classmethod
     def get_products_message(cls, basket_code=None, products=None):
+        """
+        Create a *get products from a basket* Message.
+
+        - If *basket_code* has a value, is a client request and products is ignored.
+        - If *basket_code* is null, is the response of the server and products will have values.
+
+        basket_code: UUID
+        products: List of codes from all products inside a basket (array of str)
+
+        return: Message object
+        """
         return Message(
             code=Message.GET_PRODUCTS_MESSAGE,
             content={
@@ -103,6 +182,17 @@ class MessageManager(object):
 
     @classmethod
     def get_checkout_message(cls, basket_code=None, total=None):
+        """
+        Create a *get checkout from a basket* Message.
+
+        - If *basket_code* has a value, is a client request and total is ignored.
+        - If *basket_code* is null, is the response of the server and total will have value.
+
+        basket_code: UUID
+        total: Total value of all products inside the basket (float)
+
+        return: Message object
+        """
         return Message(
             code=Message.GET_CHECKOUT_MESSAGE,
             content={
